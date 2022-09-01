233a1159b3b014298eb9dcc42b27592da70b8cdd
Wilson Valer <nederig21@gmail.com>
2022-09-01 15:57:30 -0500

    the task 9 has been edited

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,17 +6,23 @@
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
-	else
+
+	if (tree->left)
 	{
-		int left_height = binary_tree_height(tree->left);
-		int right_height = binary_tree_height(tree->right);
-
-		if (left_height >= right_height)
-			return (left_height + 1);
-		else
-			return (right_height + 1);
+		left_height = binary_tree_height(tree->left);
+		return (left_height + 1);
 	}
+
+	if (tree->right)
+	{
+		right_height = binary_tree_height(tree->right);
+		return (right_height + 1);
+	}
+	return (0);
 }
